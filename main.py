2806ac2995b9c8ea1381148fc8824fb9cb1c89a0
Thomas Oestgulen <tostgulen@gmail.com>
2020-02-26 20:57:03 +0100

    added main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 26 20:47:41 2020
+
+@author: tostg
+
+SUMMARY:
+
+Script for archiving and keeping track of versions of files in a given 
+folder.     
+"""
+
